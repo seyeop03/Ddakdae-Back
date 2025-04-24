650d780bfcc792078010c9bb9d34acbd45e5667d
seyeop03 <seyeop03@gmail.com>
2025-04-24 21:23:27 +0900

    first commit

---
+++ src/main/java/idiots/ddakdae/DdakdaeApplication.java
@@ -0,0 +1,13 @@
+package idiots.ddakdae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdakdaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdakdaeApplication.class, args);
+	}
+
+}
